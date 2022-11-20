ed35d2ef338ae1d99cb01863cd81b8de63f8c846
root <root@353bda76b1dc>
2022-11-20 22:25:52 +0000

    unbirthday

---
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -0,0 +1,22 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+
+public class unbirthday {
+	
+	public static void main(String[] args) {
+		
+	
+String birthday = JOptionPane.showInputDialog("When is your birhtday?");
+
+if (birthday.equalsIgnoreCase(11/20)) {
+	JOptionPane.showMessageDialog(null, "Happy Birthday");
+	
+}
+else {
+	JOptionPane.showMessageDialog(null, "Whish you a very merry unbirthday");
+}
+
+}
+}
