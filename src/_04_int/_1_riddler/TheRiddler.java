2b690fadefa1a8675edb68794b3f9aceca01976a
root <root@de902cbde623>
2023-02-05 20:44:35 +0000

    the riddler

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,29 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+      int score = 0; 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+      String tomRiddle = JOptionPane.showInputDialog("How does Harry Potter get rid of a rash?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+     if (tomRiddle.equalsIgnoreCase("Quit-itch")) {
+      JOptionPane.showMessageDialog(null, "Your answer is correct");
+     
+      }
 		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
+     else {
+		JOptionPane.showMessageDialog(null, "Quit-itch");
 	}
+		// 6. Add some more riddles
+     String gorilla = JOptionPane.showInputDialog("What do you call a gorilla that plays with clay?");
+     if (gorilla.equalsIgnoreCase("Hairy Potter")) {
+    	 JOptionPane.showMessageDialog(null, "Correct");
+     			
+	}
+     else {
+		JOptionPane.showMessageDialog(null, "Hairy Potter");
+	}
+		// 2. Make a pop up to show the score.
+     JOptionPane.showMessageDialog(null, "Your score is" + score);
+}
 }
 
